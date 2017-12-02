4743d6fd4f6c0a69f2f1dbe8d33e51c8ada53ce1
weiyalin <727299708@qq.com>
2017-12-02 19:52:18 +0800

    修改控制器结构

---
+++ app/Http/Controllers/Blog/TagController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Models\Tag;
+
+class TagController extends Controller
+{
+    function getTag(){
+        
+        $tagNames = Tag::getTag();
+        $tags = [];
+        if($tagNames){
+            foreach ($tagNames as $tag){
+                $tags[] = ['name' => $tag->name];
+            }
+        }
+        return responseToJson(0,'查询成功',$tags);
+    }
+    function getTagList(Request $request){
+        $per_page = intval($request->input('per_page', 10));
+        $keyword  = $request->input('keyword');
+        
+        $query = Tag::where('user_id',get_user_id());
+        if($keyword){
+            $query = Tag::where('name', 'like', $keyword . '%');
+        }
+        $students = $query->orderBy('rate', 'desc')->paginate($per_page);
+        
+        return responseToJson(0,'查询成功',$students);
+    }
+}
